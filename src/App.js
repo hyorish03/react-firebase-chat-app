f4f6396f19f41beda855e5f22c166eb7fd759e29
hyorish03 <hyorish2001@gmail.com>
2023-02-01 11:21:19 +0900

    리액트 설치 및 React-Router-Dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import "./App.css";
-import { Router, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import ChatPage from "./components/ChatPage/ChatPage";
 import LoginPage from "./components/LoginPage/LoginPage";
 import RegisterPage from "./components/RegisterPage/RegisterPage";
 
 function App() {
   return (
-    <Router>
-      <switch>
-        <Route exact path="/" component={ChatPage} />
-        <Route exact path="/Login" component={LoginPage} />
-        <Route exact path="/Register" component={RegisterPage} />
-        
-      </switch>
-    </Router>
+    <Routes>
+      <Route exact path="/" element={<ChatPage />} />
+      <Route exact path="/Login" element={<LoginPage />} />
+      <Route exact path="/Register" element={<RegisterPage />} />
+    </Routes>
   );
 }
 
--- src/components/ChatPage/ChatPage.js
+++ src/components/ChatPage/ChatPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function ChatPage() {
-  return <div></div>;
+  return <div>asd</div>;
 }
 
 export default ChatPage;
