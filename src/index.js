3a658efbe5c1d01d2f8458804b22f2352bf68fc2
hyorish03 <hyorish2001@gmail.com>
2023-02-09 18:39:07 +0900

    redux 기본

--- src/index.js
+++ src/index.js
@@ -4,11 +4,30 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import Reducer from "./redux/reducers/index";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <BrowserRouter>
-    <App />
+  <BrowserRouter
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <Provider>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
 
